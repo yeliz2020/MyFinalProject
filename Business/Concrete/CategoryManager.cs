c0a5be73e3ce236623d6514d5dbe6869d5ebc43f
Yeliz <yeliz.isler@gmail.com>
2021-02-06 23:24:06 +0300

    6 Şubat

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Business.Abstact;
+
+using DataAccess.Abstract;
+
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            //iş kodları
+            return _categoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _categoryDal.Get(c => c.CategoryId == categoryId);
+        }
+    }
+}
+
--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -8,13 +8,7 @@ using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal : IEntityRepository<Product>
+    public interface IOrderDal: IEntityRepository<Order>
     {
-      
-        
     }
-
-    
 }
-
-//code refactoring
