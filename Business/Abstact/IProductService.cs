bbd06d45123205799fb0a266d9b0bb41ac2125d5
Yeliz <yeliz.isler@gmail.com>
2021-02-11 00:02:47 +0300

    10 Şubat ders sonu

--- Business/Abstact/IProductService.cs
+++ Business/Abstact/IProductService.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Core.Utilities.Results;
+
 using Entities.Concrete;
 using Entities.DTOs;
 
@@ -10,10 +12,12 @@ namespace Business.Abstact
     public interface IProductService
     {
 
-        List<Product> GetAll();
-        List<Product> GetAllByCategoryId(int id);
-        List<Product> GetByUnitPrice(decimal min, decimal max);
-        List<ProductDetailDto> GetProductDetails();
+        IDataResult<List<Product>> GetAll();
+        IDataResult<List<Product>> GetAllByCategoryId(int id);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IDataResult<Product> GetById(int productId);
+        IResult Add(Product product);
 
     }
 }
