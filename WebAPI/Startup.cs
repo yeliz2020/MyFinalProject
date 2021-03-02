ff290927bc32e41f9b682d48a4867acc68568d50
Yeliz <yeliz.isler@gmail.com>
2021-03-02 17:47:41 +0300

    create servis kodu eklendi

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Business.Abstact;
 using Business.Concrete;
 
+using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
 
@@ -56,6 +57,7 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
+            ServiceTool.Create(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
